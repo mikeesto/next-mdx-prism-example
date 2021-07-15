9105075e33bae663c5367838c3fb0fc25d7d98c3
Michael Esteban <mickel13@gmail.com>
2021-07-15 17:04:35 +1000

    add everything

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
-}
+};
+
+// next.config.js
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [require("remark-prism")],
+  },
+});
+
+module.exports = withMDX({
+  pageExtensions: ["js", "jsx", "mdx"],
+});
